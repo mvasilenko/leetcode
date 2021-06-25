ecd616e86d655214a98f706f9d41684a6b751d2d
Mikhail Vasilenko <mvasilenko@gmail.com>
2021-06-25 14:07:31 +0300

    longest common prefix - naive

---
+++ longest_common_prefix.py
@@ -0,0 +1,54 @@
+import math
+import unittest
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        shortest = math.inf
+        for s in strs:
+            shortest_len = min(shortest, len(s))
+        # print(shortest_len)
+        continue_search = True
+        while len(prefix) < shortest_len and continue_search and shortest_len > 0:
+            # print(f"len(prefix)={len(prefix)}")
+            if len(strs[0]) > len(prefix):
+                #print(f"prefix={prefix} strs[0]={strs[0]}")
+                prefix += strs[0][len(prefix)]
+            else:
+                break
+            for s in strs:
+                #print(f"s[0:len(prefix)]={s[0:len(prefix)]} prefix={prefix}")
+                if not s.startswith(prefix):
+                    # print("break")
+                    continue_search = False
+                    if len(prefix) > 0:
+                        prefix = prefix[:-1:]
+                    #print(f"return {prefix}")
+                    break
+
+        return prefix
+
+
+class TestSolution(unittest.TestCase):
+    solution = Solution()
+
+    def test_1(self):
+        self.assertEqual(self.solution.longestCommonPrefix(["dog", "racecar", "car"]), "")
+
+    def test_2(self):
+        self.assertEqual(self.solution.longestCommonPrefix(["flower", "flow", "fl"]), "fl")
+
+    def test_3(self):
+        self.assertEqual(self.solution.longestCommonPrefix([""]), "")
+
+    def test_4(self):
+        self.assertEqual(self.solution.longestCommonPrefix(["a"]), "a")
+
+    def test_5(self):
+        self.assertEqual(self.solution.longestCommonPrefix(["", "b"]), "")
+
+
+if __name__ == "__main__":
+    unittest.main()
