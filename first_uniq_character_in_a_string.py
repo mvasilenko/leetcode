a05f13b8e0c624df80f4a1693ae9d030300453e0
Mikhail Vasilenko <mvasilenko@gmail.com>
2022-01-03 14:40:30 +0200

    reverse int, first uniq char in a string

---
+++ first_uniq_character_in_a_string.py
@@ -0,0 +1,30 @@
+import unittest
+import collections
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d = collections.defaultdict(int)
+        for c in s:
+            d[c] += 1
+        for i, c in enumerate(s):
+            if d[c] == 1:
+                return i
+        return 0
+
+
+class TestSolution(unittest.TestCase):
+    solution = Solution()
+
+    def test_1(self):
+        self.assertEqual(self.solution.firstUniqChar("leetcode"), 0)
+
+    def test_2(self):
+        self.assertEqual(self.solution.firstUniqChar("loveleetcode"), 2)
+
+    def test_3(self):
+        self.assertEqual(self.solution.firstUniqChar("aabb"), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- reverse_integer.py
+++ reverse_integer.py
@@ -3,6 +3,16 @@ import unittest
 
 class Solution:
     def reverse(self, x: int) -> int:
+        res = 0
+        if x > 0:
+            res = int(str(x)[::-1])
+        else:
+            res = -1 * int(str(-1 * x)[::-1])
+        if res > pow(2, 31)-1 or res < -1 * pow(2, 31):
+            return 0
+        return res
+
+    def reverse_1(self, x: int) -> int:
         sign = -1 if x < 0 else 1
         x_str = str(abs(x))[::-1]
         x_int = int(x_str)
