fc0e15cc366e0477c89eb44bdfd796cd0fdf5494
Mikhail Vasilenko <mvasilenko@gmail.com>
2022-05-07 15:00:07 +0100

    add naive version

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -4,30 +4,45 @@ from typing import List
 
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix = ""
-        shortest = math.inf
-        for s in strs:
-            shortest_len = min(shortest, len(s))
-        # print(shortest_len)
-        continue_search = True
-        while len(prefix) < shortest_len and continue_search and shortest_len > 0:
-            # print(f"len(prefix)={len(prefix)}")
-            if len(strs[0]) > len(prefix):
-                #print(f"prefix={prefix} strs[0]={strs[0]}")
-                prefix += strs[0][len(prefix)]
-            else:
-                break
+    def longestCommonPrefixNaive(self, strs: List[str]) -> str:
+        def longestCommonPrefix(self, strs: List[str]) -> str:
+            prefix = ""
+            shortest = math.inf
             for s in strs:
-                #print(f"s[0:len(prefix)]={s[0:len(prefix)]} prefix={prefix}")
-                if not s.startswith(prefix):
-                    # print("break")
-                    continue_search = False
-                    if len(prefix) > 0:
-                        prefix = prefix[:-1:]
-                    #print(f"return {prefix}")
+                shortest_len = min(shortest, len(s))
+            # print(shortest_len)
+            continue_search = True
+            while len(prefix) < shortest_len and continue_search and shortest_len > 0:
+                # print(f"len(prefix)={len(prefix)}")
+                if len(strs[0]) > len(prefix):
+                    prefix += strs[0][len(prefix)]
+                else:
                     break
+                for s in strs:
+                    # print(f"s[0:len(prefix)]={s[0:len(prefix)]} prefix={prefix}")
+                    if not s.startswith(prefix):
+                        # print("break")
+                        continue_search = False
+                        if len(prefix) > 0:
+                            prefix = prefix[:-1:]
+                        # print(f"return {prefix}")
+                        break
 
+            return prefix
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        prefix = ""
+        shortest = min(strs, key=len)
+        for i in range(len(shortest)):
+            for s in strs:
+                if s[i] != shortest[i]:
+                    prefix = s[:i]
+                    if i > 0:
+                        return prefix
+                    else:
+                        return ""
         return prefix
 
 
